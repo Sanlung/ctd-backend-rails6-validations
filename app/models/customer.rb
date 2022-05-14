6b1db479f443f4ea78b46976f93d8b56bdaf3048
Chung Kao <chungckao@gmail.com>
2022-05-14 18:34:11 +0800

    Edit Customer class to chain the vaidates presence statement

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,7 @@
 class Customer < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone, presence: true
+  validates :first_name, :last_name, :phone, :email, presence: true
   validates :phone, numericality: { only_integer: true }
   validates :phone, length: { is: 10 }
-  validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   def full_name
